0a008430622715aca670becd80dd0a1c2cda7181
animeshsingh04 <pandey>
2020-05-17 19:13:36 +0530

    added validation

---
+++ build/precache-manifest.ba7a98ef16fac920b7af823f5af692df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0226421283bde0e96a1f7cdf3f8d7a2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b51edfcda04b0e5528a4",
+    "url": "./static/css/main.fb8dabfc.chunk.css"
+  },
+  {
+    "revision": "14bcb1de7d5550185d04",
+    "url": "./static/js/2.95d6bbb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.95d6bbb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51edfcda04b0e5528a4",
+    "url": "./static/js/main.9fe27af4.chunk.js"
+  },
+  {
+    "revision": "08c4b19a7c02c13909e4",
+    "url": "./static/js/runtime-main.eb4e0825.js"
+  }
+]);
